eac351e57711da65a9f848b14c6033086f28961c
Animesh Pandey <animeshpandey1726@gmail.com>
2026-04-28 17:20:45 +0530

    handled CORS for API call from frontend

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.api.routes import health
 from app.db.session import engine
 from app.db.models import Base
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Healthcare AI Backend",
@@ -12,6 +13,14 @@ Base.metadata.create_all(bind=engine)
 
 # Include routes
 app.include_router(health.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # later restrict this
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
